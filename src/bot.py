33c02b15130d9a448a3a1b210d80402efc1ceebe
Hykilpikonna <me@hydev.org>
2020-11-22 11:50:24 -0500

    [+] Create command help

---
+++ src/bot.py
@@ -0,0 +1,20 @@
+
+helpMsg = """
+Welcome! This bot monitors http changes!
+
+/start - Start the bot
+
+**Management Commands**
+/ls - List the http requests you've created
+/touch - Create a http request
+/rm - Delete a http request
+/mv - Rename a http request
+
+**Configuration Commands**
+/nano - Edit a http request
+/interval - Change the interval between the updates of a http request
+
+**Start/Stop Commands**
+/enable - Start listening to a http request
+/stop - Stop listening to a http request
+"""
---
+++ src/commands.md
@@ -0,0 +1,9 @@
+start - Start the bot
+ls - List the http requests you've created
+touch - Create a http request
+rm - Delete a http request
+mv - Rename a http request
+nano - Edit a http request
+interval - Change the interval between the updates of a http request
+enable - Start listening to a http request
+stop - Stop listening to a http request
