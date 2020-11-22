34ec2732e812e5023ea4bbdcf7603deb77f5c42f
Hykilpikonna <me@hydev.org>
2020-11-22 12:45:17 -0500

    [+] Create database.save

--- src/database.py
+++ src/database.py
@@ -1,4 +1,14 @@
+import json
+
+from src.bot import dbPath
+
+
 class Database:
     users = []
     userRequests = {}
     userStatus = {}
+
+    def save(self):
+        f = open(dbPath, 'w')
+        f.write(json.dumps(self))
+        f.close()
