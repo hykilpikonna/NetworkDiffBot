8ee94395790a6ee1f83a3d7ca45dcb1a9df373e9
Hykilpikonna <me@hydev.org>
2020-11-27 10:37:15 -0500

    [O] Exit on keyboard interrupt

--- src/bot.py
+++ src/bot.py
@@ -11,6 +11,7 @@ from src.utils import createCommand
 if __name__ == '__main__':
     #  Initialize logger
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.getLogger('apscheduler.executors.default').propagate = False
 
     # Create bot
     bot = telegram.Bot(token=token)
@@ -34,4 +35,10 @@ if __name__ == '__main__':
 
     # Keep alive
     while True:
-        time.sleep(5)
+        try:
+            time.sleep(5)
+        except KeyboardInterrupt:
+            print("Exiting...")
+            updater.stop()
+            dispatcher.stop()
+            exit(0)
